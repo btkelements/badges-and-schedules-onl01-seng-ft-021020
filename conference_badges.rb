29b102397504f37a0ccc228746cd1c3a1ebeabad
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-18 02:40:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,4 +3,20 @@ def badge_maker(name)
 end
 
 def batch_badge_creator
-  
+  badge_messages = []
+  speakers.each do |speaker|
+    message = badge_maker(speaker)
+    badge_messages << message
+  end
+  badge_messages
+end
+
+def assign_rooms(speakers)
+  room_number = 1
+  room_messages = []
+  speakers.each do |speaker|
+    room_messages << "Hello, #{name}! You'll be assigned to room #{room_number}!"
+    room_number += 1
+  end
+  room_messages
+end
